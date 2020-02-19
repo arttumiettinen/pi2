197eaddc225825f835053c3aed3ab09043620af8
Arttu Miettinen <arttu.miettinen@psi.ch>
2020-02-19 16:06:22 +0100

    Fixed problem that caused pilib be always rebuilt.

--- pilib/create_commit_info.cmd
+++ pilib/create_commit_info.cmd
@@ -1,6 +1,23 @@
+@echo off
+SetLocal EnableDelayedExpansion
 
-git describe --dirty --always --tags > commit_info.txt
-set /p var=<commit_info.txt
-echo std::string VERSION = ^"%var%^"; > commit_info.txt
+IF NOT EXIST commit_info.txt (
+	set /p OLD=
+) ELSE (
+	set /p OLD=<commit_info.txt
+)
 
+
+git describe --dirty --always --tags > commit_info_temp.txt
+set /p NEW=<commit_info_temp.txt
+echo std::string VERSION = ^"%NEW%^"; > commit_info_temp.txt
+set /p NEW=<commit_info_temp.txt
+
+REM echo %OLD%
+REM echo %NEW%
+
+IF NOT !OLD!==!NEW! (
+	echo Version string changed
+	echo %NEW%>commit_info.txt
+)
 
