6214e7967bfedee8aec0e56514da565fc55567c4
Arttu Miettinen <arttu.i.miettinen@jyu.fi>
2021-10-21 12:23:35 +0300

    Added direct reconstruction mode that skips -log and/or Paganin.

--- itl2/tomo/fbp.cpp
+++ itl2/tomo/fbp.cpp
@@ -218,6 +218,9 @@ namespace itl2
 		case PhaseMode::Paganin:
 			paganinSlice(slice, padType, padFraction, objectSourceDistance, objectCameraDistance, delta, mu);
 			break;
+		case PhaseMode::Direct:
+			// The data is already in -ln(I/I0) format or equivalent, so do not do anything.
+			break;
 		default:
 			throw ITLException("Unsupported phase retrieval mode.");
 		}
@@ -235,6 +238,9 @@ namespace itl2
 			break;
 		case PhaseMode::Paganin:
 			paganin(transmissionProjections, padType, padFraction, objectSourceDistance, objectCameraDistance, delta, mu);
+			break;
+		case PhaseMode::Direct:
+			// The data is already in -ln(I/I0) format or equivalent, so do not do anything.
 			break;
 		default:
 			throw ITLException("Unsupported phase retrieval mode.");
