bacac3fb3c305a8b2d93397172baaba9abbdb812
Arttu Ilari Miettinen <arilmiet@jyu.fi>
2023-05-04 14:32:22 +0300

    Working on commit info system (Windows)

--- pilib/create_commit_info.cmd
+++ pilib/create_commit_info.cmd
@@ -2,22 +2,30 @@
 SetLocal EnableDelayedExpansion
 
 IF NOT EXIST commit_info.txt (
-	set /p OLD=
+	set OLD=
 ) ELSE (
 	set /p OLD=<commit_info.txt
 )
 
 
-git describe --dirty --always --tags > commit_info_temp.txt
-set /p NEW=<commit_info_temp.txt
+set NEW=unknown
+
+where git >nul 2>nul
+IF !ERRORLEVEL! EQU 0 (
+	git rev-parse --is-inside-work-tree >nul 2>nul
+    IF !ERRORLEVEL! EQU 0 (
+		git describe --dirty --always --tags > commit_info_temp.txt
+		set /p NEW=<commit_info_temp.txt
+	)
+)
+
 echo std::string VERSION = ^"%NEW%^"; > commit_info_temp.txt
 set /p NEW=<commit_info_temp.txt
 
-REM echo %OLD%
-REM echo %NEW%
+rem echo %OLD%
+rem echo %NEW%
 
 IF NOT !OLD!==!NEW! (
 	echo Version string changed
 	echo %NEW%>commit_info.txt
 )
-
