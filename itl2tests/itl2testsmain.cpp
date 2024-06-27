107a7856f9f5448b839eb292a023e34f179dbbf2
brokkoli71 <brokkoli71b@gmail.com>
2024-06-27 19:43:26 +0200

    wip

--- itl2tests/itl2testsmain.cpp
+++ itl2tests/itl2testsmain.cpp
@@ -247,7 +247,7 @@ int main()
 	//test(itl2::lz4::tests::lz4io, "LZ4");
 	//test(itl2::lz4::tests::lz4blockIo, "LZ4 block");
 	//
-	//test(itl2::nn5::tests::nn5Metadata, "NN5 metadata");
+	test(itl2::nn5::tests::nn5Metadata, "NN5 metadata");
 	//test(itl2::nn5::tests::nn5io, "NN5 I/O");
 	//test(itl2::nn5::tests::nn5BlockIo, "NN5 block I/O");
 	//test(itl2::nn5::tests::concurrency, "NN5 concurrent I/O");
@@ -314,7 +314,7 @@ int main()
 
 	
 
-	testReport();
+	itl2::testReport();
 
 	//cout << "Press return to exit..." << endl;
 	//char dummy;
