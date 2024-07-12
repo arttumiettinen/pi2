74440764343cfdb3e3d7d12639f8b79db4144d79
brokkoli71 <brokkoli71b@gmail.com>
2024-07-12 18:43:29 +0200

    create pytests

--- test_zarr.py
+++ test_zarr.py
@@ -1,6 +1,9 @@
 import os
 import shutil
 import sys
+
+import pytest
+
 print(os.getcwd())
 if os.getcwd().endswith("/venv/bin"):
     os.chdir("../..")
@@ -19,20 +22,19 @@ def output_file(name):
 #shutil.rmtree("testoutput", ignore_errors=True)
 
 arr = np.arange(10*10*10, dtype=np.float32).reshape(10,10,10)
-w = 2
-h = 3
-d = 4
+w = 3
+h = 5
+d = 7
 arr = arr[:w, :h, :d]
-chunk_shape = [w,h,d]
-#chunk_shape = [1,1,2]
-def pi2_write():
+
+def pi2_write(chunk_shape):
     name = "test.zarr"
     shutil.rmtree(output_file(name), ignore_errors=True)
     write_img = pi2.newimage(pi2py2.ImageDataType.UInt32, [h, w, d])
     write_img.set_data(arr.transpose(1, 0, 2))
     pi2.writezarr(write_img, output_file(name), chunk_shape)
 
-def zarrita_write():
+def zarrita_write(chunk_shape):
     name = "zarrita.zarr"
     shutil.rmtree(output_file(name), ignore_errors=True)
     store = zarrita.LocalStore(output_file(name))
@@ -51,7 +53,7 @@ def zarrita_write():
 def pi2_read(name):
     read_img = pi2.read(output_file(name))
     read_arr = read_img.get_data()
-    return read_arr
+    return read_arr.transpose(1, 0, 2)
 
 def zarrita_read(name):
     store = zarrita.LocalStore(output_file(name))
@@ -60,47 +62,33 @@ def zarrita_read(name):
     return read_arr
 
 
-
-
-
-#todo test with no chunk_shape
-
-# passed
-def test_pi2_to_pi2():
-    pi2_write()
+@pytest.mark.parametrize("chunk_shape", [[1,1,1], [1,1,d], [1,h,d], [w,h,d]])
+def test_pi2_to_pi2(chunk_shape):
+    pi2_write(chunk_shape)
     read_arr = pi2_read("test.zarr")
     assert np.array_equal(arr, read_arr), "read_arr:\n " + str(read_arr) + " \n\narr:\n " + str(arr)
     print("passed")
 
-# passed
-def test_zarrita_to_zarrita():
-    zarrita_write()
+@pytest.mark.parametrize("chunk_shape", [[1,1,1], [1,1,d], [1,h,d], [w,h,d]])
+def test_zarrita_to_zarrita(chunk_shape):
+    zarrita_write(chunk_shape)
     read_arr = zarrita_read("zarrita.zarr")
     assert np.array_equal(arr, read_arr), "read_arr:\n " + str(read_arr) + " \n\narr:\n " + str(arr)
     print("passed")
 
-# # passed with chunk_shape = shape
-# def test_pi2_to_zarrita_transpose_corrected():
-#     pi2_write()
-#     read_arr = zarrita_read("test.zarr")
-#     assert np.array_equal(arr, read_arr) or np.array_equal(arr, read_arr.transpose(1, 0, 2)), "read_arr:\n " + str(read_arr) + " \n\narr:\n " + str(arr)
-#     print("passed")
-
-# works for chunk_shape = [1,1,1]
-def test_pi2_to_zarrita():
-    pi2_write()
-    zarrita_write()
+@pytest.mark.parametrize("chunk_shape", [[1,1,1], [1,1,d], [1,h,d], [w,h,d]])
+def test_pi2_to_zarrita(chunk_shape):
+    pi2_write(chunk_shape)
     read_arr = zarrita_read("test.zarr")
     assert np.array_equal(arr, read_arr), "read_arr:\n " + str(read_arr) + " \n\narr:\n " + str(arr)
     print("passed")
 
-def test_zarrita_to_pi2():
-    zarrita_write()
-    read_arr = pi2_read("zarrita.zarr").transpose(1, 0, 2)
+@pytest.mark.parametrize("chunk_shape", [[1,1,1], [1,1,d], [1,h,d], [w,h,d]])
+def test_zarrita_to_pi2(chunk_shape):
+    zarrita_write(chunk_shape)
+    read_arr = pi2_read("zarrita.zarr")
     # write_img = pi2.newimage(pi2py2.ImageDataType.FLOAT32, w, h, d)
     # write_img.set_data(read_arr)
     # pi2.writezarr(write_img, output_file("test.zarr"), chunk_shape)
     assert np.array_equal(arr, read_arr), "read_arr:\n " + str(read_arr) + " \n\narr:\n " + str(arr)
     print("passed")
-
-test_zarrita_to_pi2()
