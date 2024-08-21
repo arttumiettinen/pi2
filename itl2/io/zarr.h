2ea22b86b212e94a063c24b1b0d2d9263c3c90bd
brokkoli71 <brokkoli71b@gmail.com>
2024-08-21 16:34:32 +0200

    fix template bug

--- itl2/io/zarr.h
+++ itl2/io/zarr.h
@@ -13,7 +13,7 @@
 
 namespace itl2
 {
-	using std::cout, std::endl;
+	using std::cout, std::endl, std::ifstream;
 	namespace io
 	{
 		// Forward declaration of io::getInfo to avoid header loop.
@@ -56,7 +56,29 @@ namespace itl2
 			Writes Zarr metadata file.
 			*/
 			template<typename pixel_t>
-			void writeMetadata(const std::string& path, const Vec3c& shape, ImageDataType dataType, const ZarrMetadata<pixel_t>& metadata);
+			void writeMetadata(const std::string& path, const Vec3c& shape, ImageDataType dataType, const ZarrMetadata<pixel_t>& metadata)
+			{
+				nlohmann::json j =
+					{
+						{ "zarr_format", 3 },//zarr updated
+						{ "node_type", "array" },//zarr updated
+						{ "shape", { shape[0], shape[1], shape[2] }},//zarr updated
+						{ "data_type", toString(dataType) },//zarr updated
+						{ "chunk_grid", {{ "name", "regular" }, { "configuration", {{ "chunk_shape", { metadata.chunkSize[0], metadata.chunkSize[1], metadata.chunkSize[2] }}}}}},
+						{ "chunk_key_encoding", {{ "name", "default" }, { "configuration", {{ "separator", metadata.separator }}}}},
+						{ "fill_value", metadata.fillValue },
+						{ "codecs", {}}
+					};
+				for (auto& codec : metadata.codecs)
+				{
+					j["codecs"].push_back(codec.toJSON());
+				}
+				// TODO: allow other chunk_key_encoding
+				// TODO: optional parameters
+				string metadataFilename = zarr::internals::zarrMetadataFilename(path);
+				std::ofstream of(metadataFilename, std::ios_base::trunc | std::ios_base::out);
+				of << std::setw(4) << j << endl;
+			}
 
 			inline std::string chunkFile(const std::string& path, size_t dimensionality, const Vec3c& chunkIndex, const string& separator)
 			{
@@ -226,6 +248,204 @@ namespace itl2
 				});
 			}
 
+			/*
+			 * getInfo will return false if the provided data is either no zarr data or has a configuration not supported by this implementation
+			 * getInfo will throw ITLException if zarr data is provided but does not match zarr specifications
+			 */
+			template<typename pixel_t>
+			inline bool getInfo(const std::string& path,
+				Vec3c& shape,
+				ImageDataType& dataType,
+				ZarrMetadata <pixel_t>& metadata,
+				std::string& reason)
+			{
+				shape = Vec3c();
+				dataType = ImageDataType::Unknown;
+
+				// Check that metadata file exists.
+				string metadataFilename = zarr::internals::zarrMetadataFilename(path);
+				if (!fs::exists(metadataFilename))
+				{
+					reason = "Metadata file does not exist.";
+					return false;
+				}
+
+				// Read metadata and populate shape and pixel data type.
+				ifstream in(metadataFilename);
+				nlohmann::json j;
+
+				try
+				{
+					in >> j;
+				}
+
+				catch (nlohmann::json::exception ex)
+				{
+					reason = string("Unable to parse zarr metadata: ") + ex.what();
+					return false;
+				}
+				if (!j.contains("zarr_format"))
+				{
+					throw ITLException("zarr_format is missing in zarr metadata.");
+				}
+				else
+				{
+					int zarr_format = j["zarr_format"].get<int>();
+					if (zarr_format != 3)
+					{
+						reason = "This zarr implementation supports only zarr_format 3.";
+						return false;
+					}
+				}
+				if (!j.contains("node_type"))
+				{
+					throw ITLException("node_type is missing in zarr metadata.");
+				}
+				else
+				{
+					string node_type = j["node_type"].get<string>();
+					if (node_type != "array")
+					{
+						throw ITLException("This zarr implementation supports only node_type array.");
+					}
+				}
+				if (!j.contains("shape"))
+				{
+					throw ITLException("shape is missing in zarr metadata.");
+				}
+
+				auto dims = j["shape"];
+				if (dims.size() > 3)
+				{
+					reason = "This zarr implementation supports only 1-, 2-, or 3-dimensional datasets.";
+					return false;
+				}
+
+				//transpose xyz -> yxz
+				shape = Vec3c(1, 1, 1);
+				shape[0] = dims[0].get<size_t>();
+				if (dims.size() >= 2)
+					shape[1] = dims[1].get<size_t>();
+				if (dims.size() >= 3)
+					shape[2] = dims[2].get<size_t>();
+
+				if (!j.contains("data_type"))
+				{
+					throw ITLException("data_type is missing in zarr metadata.");
+				}
+				try
+				{
+					string dataTypeString = j["data_type"].get<string>();
+					dataType = fromString<ImageDataType>(dataTypeString);
+					if (dataType == ImageDataType::Unknown)
+					{
+						throw ITLException("Could not identify datatype: " + dataTypeString);
+					}
+				}
+				catch (ITLException& e)
+				{
+					reason = e.message();
+					return false;
+				}
+
+				if (!j.contains("chunk_grid")
+					|| !j["chunk_grid"].contains("configuration")
+					|| !j["chunk_grid"]["configuration"].contains("chunk_shape")
+					|| !j["chunk_grid"].contains("name")
+					|| j["chunk_grid"]["name"].get<string>() != "regular"
+					)
+				{
+					throw ITLException("chunk_grid is missing or malformed in zarr metadata.");
+				}
+				else
+				{
+					auto chunkDims = j["chunk_grid"]["configuration"]["chunk_shape"];
+					if (chunkDims.size() != dims.size())
+					{
+						throw ITLException("Chunk shape and dataset shape contain different number of elements.");
+					}
+
+					metadata.chunkSize = Vec3c(1, 1, 1);
+					metadata.chunkSize[0] = chunkDims[0].get<size_t>();
+					if (chunkDims.size() >= 2)
+						metadata.chunkSize[1] = chunkDims[1].get<size_t>();
+					if (chunkDims.size() >= 3)
+						metadata.chunkSize[2] = chunkDims[2].get<size_t>();
+				}
+
+				if (!j.contains("chunk_key_encoding"))
+				{
+					throw ITLException("chunk_key_encoding is missing in zarr metadata.");
+				}
+				else
+				{
+					if (!j["chunk_key_encoding"].contains("name"))
+					{
+						throw ITLException("chunk_key_encoding name is missing in zarr metadata.");
+					}
+					if (j["chunk_key_encoding"]["name"].get<string>() != "default")
+					{
+						reason = "This zarr implementation supports only default chunk_key_encoding.";
+						return false;
+					}
+					if (j["chunk_key_encoding"].contains("configuration")
+						&& !j["chunk_key_encoding"]["configuration"].contains("separator"))
+					{
+						throw ITLException("chunk_key_encoding configuration separator is missing in zarr metadata");
+					}
+					else
+					{
+						metadata.separator = j["chunk_key_encoding"]["configuration"]["separator"].get<string>();
+					}
+				}
+
+				if (!j.contains("fill_value"))
+				{
+					throw ITLException("fill_value is missing in zarr metadata.");
+				}
+				else
+				{
+					try
+					{
+						nlohmann::json::value_t fillValue_t = j["fill_value"].type();
+						switch (fillValue_t)
+						{
+						case nlohmann::json::value_t::number_float:
+							metadata.fillValue = (pixel_t)j["fill_value"];
+							break;
+						case nlohmann::json::value_t::number_integer:
+							metadata.fillValue = (pixel_t)j["fill_value"].get<int>();
+							break;
+						case nlohmann::json::value_t::number_unsigned:
+							metadata.fillValue = (pixel_t)j["fill_value"].get<uint>();
+							break;
+						default:
+							metadata.fillValue = (pixel_t)j["fill_value"].get<int>();
+						}
+					}
+					catch (nlohmann::json::exception ex)
+					{
+						reason =
+							string("Unable to parse fill_value in zarr metadata (this implementation only supports floats and integers): ") +
+								ex.what();
+						return false;
+					}
+				}
+
+				if (!j.contains("codecs"))
+				{
+					throw ITLException("codecs is missing in zarr metadata.");
+				}
+				else
+				{
+					if (!codecs::fromJSON(metadata.codecs, j["codecs"], reason))
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+
 			/**
 			Checks that provided zarr information is correct and writes metadata.
 			@param deleteOldData Contents of the dataset are usually deleted when a write process is started. Set this to false to delete only if the path contains an incompatible dataset, but keep the dataset if it seems to be the same than the current image.
@@ -234,22 +454,71 @@ namespace itl2
 			void handleExisting(const Vec3c& imageDimensions,
 				ImageDataType imageDataType,
 				const std::string& path,
-				const ZarrMetadata<pixel_t>& metadata,
-				bool deleteOldData);
+				const ZarrMetadata <pixel_t>& metadata,
+				bool deleteOldData)
+			{
+				// Delete old dataset if it exists.
+				if (fs::exists(path))
+				{
+					Vec3c oldDimensions;
+					ImageDataType oldDataType;
+					ZarrMetadata<pixel_t> oldMetadata;
+					string dummyReason;
+					string zarrReason;
 
+					bool isZarrImage = false;
+					try
+					{
+						isZarrImage = getInfo(path, oldDimensions, oldDataType, oldMetadata, zarrReason);
+					}
+					catch (ITLException& e)
+					{
+						zarrReason = e.message();
+					}
+					if (!isZarrImage)
+					{
+						// The path does not contain a Zarr dataset.
+						// If it is no known image, do not delete it.
+						bool isKnownImage = false;
+						try
+						{
+							isKnownImage = io::getInfo(path, oldDimensions, oldDataType, dummyReason);
+						}
+						catch (ITLException& e)
+						{}
+						if (!isKnownImage)
+							throw ITLException(string("Unable to write a Zarr as the output folder already exists but cannot be verified to be an image: ") + path
+								+ " Consider removing the existing dataset manually. Reason why could not read as zarr: " + zarrReason);
+
+						// Here the path does not contain Zarr but contains an image of known type.
+						// Delete the old image.
+						fs::remove_all(path);
+					}
+					else if (oldDimensions == imageDimensions &&
+						oldDataType == imageDataType &&
+						oldMetadata == metadata)
+					{
+						// The path contains a compatible Zarr dataset.
+						// Delete it if we are not continuing a concurrent write.
+						if (!fs::exists(internals::concurrentTagFile(path)))
+							if (deleteOldData)
+								fs::remove_all(path);
+					}
+					else
+					{
+						// The path contains an incompatible Zarr dataset. Delete it.
+						if (fs::exists(internals::concurrentTagFile(path)) && !deleteOldData)
+							throw ITLException(string("The output folder contains an incompatible zarr dataset that is currently being processed concurrently."));
+						fs::remove_all(path);
+					}
+				}
+			}
 		}
-
-		template<typename pixel_t>
-		bool getInfo(const std::string& path,
-			Vec3c& shape,
-			ImageDataType& dataType,
-			ZarrMetadata<pixel_t>& metadata,
-			std::string& reason);
 
 		inline bool getInfo(const std::string& path, Vec3c& shape, ImageDataType& dataType, std::string& reason)
 		{
 			ZarrMetadata<u_int16_t> dummyMetadata;
-			return getInfo(path, shape, dataType, dummyMetadata, reason);
+			return zarr::internals::getInfo(path, shape, dataType, dummyMetadata, reason);
 		}
 
 		inline const Vec3c DEFAULT_CHUNK_SIZE = Vec3c(1536, 1536, 1536);
@@ -326,7 +595,7 @@ namespace itl2
 			ZarrMetadata<pixel_t> metadata;
 			string reason;
 			std::cout << "readBlock" << std::endl;
-			if (!itl2::zarr::getInfo(path, datasetShape, dataType, metadata, reason))
+			if (!itl2::zarr::internals::getInfo(path, datasetShape, dataType, metadata, reason))
 				throw ITLException(string("Unable to read zarr dataset: ") + reason);
 
 			if (dataType != img.dataType())
@@ -354,7 +623,7 @@ namespace itl2
 			ImageDataType dataType;
 			ZarrMetadata<pixel_t> metadata;
 			string reason;
-			if (!itl2::zarr::getInfo(path, dimensions, dataType, metadata, reason))
+			if (!itl2::zarr::internals::getInfo(path, dimensions, dataType, metadata, reason))
 				throw ITLException(string("Unable to read zarr dataset: ") + reason);
 			img.ensureSize(dimensions);
 
